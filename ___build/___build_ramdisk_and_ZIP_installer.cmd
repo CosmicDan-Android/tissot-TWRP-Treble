a82b4c7d98bb0627e4e1390388380eb6f4ef0fff
CosmicDan <dconnolly.au@gmail.com>
2018-05-22 19:17:42 +1000

    Update to 1.1

--- ___build/___build_ramdisk_and_ZIP_installer.cmd
+++ ___build/___build_ramdisk_and_ZIP_installer.cmd
@@ -1,6 +1,7 @@
+SET ZIP_NAME=Twrp-recoveryInstaller-3.2.1-2-oreo-TrebleManager_1.1.zip
 cd /d "%~dp0"
 :: Delete old installer ZIP
-del Twrp-recoveryInstaller-3.2.1-2-oreo-Treble.zip >nul
+del Twrp-*.zip >nul
 :: Pack Aroma resources
 cd "%~dp0\.."
 del ramdisk-recovery\treble_manager\aroma_res.zip
@@ -11,7 +12,7 @@ cd /d "%~dp0"
 call repack_img.cmd
 :: Create installer image
 cd /d "%~dp0\.."
-zip -r -1 "%~dp0\Twrp-recoveryInstaller-3.2.1-2-oreo-Treble.zip" * -x "___*" -x "ramdisk-recovery/*"
+zip -r -1 "%~dp0\%ZIP_NAME%" * -x "___*" -x "ramdisk-recovery/*"
 :: Cleanup
 del ramdisk-recovery.img
 del ramdisk-recovery\treble_manager\aroma_res.zip
--- ramdisk-recovery/sbin/treble_manager_launch.sh
+++ ramdisk-recovery/sbin/treble_manager_launch.sh
@@ -1,2 +1,4 @@
 #!/sbin/sh
 /treble_manager/aroma 1 0 /treble_manager/aroma_res.zip >/tmp/aroma_log.txt
+cp -f /treble_manager/aroma_res.zip.log.txt /sdcard1/treble_manager.log
+reboot recovery
